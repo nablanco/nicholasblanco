65f3ad46731aee177f46482955a19cff1dd67598
Nicholas Blanco <nablanco92@gmail.com>
2022-08-24 15:24:14 -0500

    Add toolbar layout

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,73 @@
 import React from "react";
+import styled from "styled-components";
+import { FaBars } from "react-icons/fa";
 
+const StyledNavbar = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: baseline;
+  align-items: center;
+  /* position: fixed;
+  top: 0;
+  z-index: 999; */
+  width: 100%;
+  height: 80px;
+  font-size: 32px;
+  border-bottom: 1px;
+`;
+
+const NavName = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 15px;
+  color: #15dba3;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-self: flex-end;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const NavBurger = styled(FaBars)`
+  color: #808080;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  &.active {
+    color: #000000;
+  }
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const NavLink = styled.div`
+  display: none;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    font-size: 18px;
+  }
+`;
 const Navbar = () => {
-  return <></>;
+  return (
+    <StyledNavbar>
+      <NavName>Nicholas Blanco</NavName>
+      <NavMenu>
+        <NavBurger />
+        <NavLink>Experience</NavLink>
+        <NavLink>Education</NavLink>
+        <NavLink>Projects</NavLink>
+        <NavLink>Courses/Certificates</NavLink>
+      </NavMenu>
+    </StyledNavbar>
+  );
 };
 
 export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
 
 import Hero from "../components/Hero/Hero";
 import Resume from "../components/Resume/Resume";
+import Navbar from "../components/Navbar/Navbar";
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  width: 100%;
+  position: relative;
+  background-color: #131518;
+  color: hsl(217, 12%, 63%);
+  font-family: 'Overpass', sans-serif;
+}
+`;
 
 const IndexPage = () => {
   return (
@@ -13,6 +29,8 @@ const IndexPage = () => {
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <div id="home" className="content">
         <title>Nicholas Blanco</title>
+        <Navbar />
+        <GlobalStyle />
         <Hero />
         <Resume />
       </div>
