8e3530a6d92a3783dad07a54ce96c27ad79646b1
Nick Blanco <nablanco92@gmail.com>
2022-09-01 18:12:58 -0500

    Add project styling for desktop, remove resume

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,27 +7,37 @@ const HeroContainer = styled.div`
   flex-flow: column nowrap;
   justify-content: center;
   align-items: center;
-  width: auto;
   text-align: center;
   margin: 90px 0px 40px 0px;
 `;
 const HeroText = styled.div`
-  text-align: center;
+  /* text-align: center; */
   width: 268px;
-  margin-bottom: 25px;
-  @media screen and (min-wdith: 768px) {
-    width: 550px;
+  margin-bottom: 30px;
+  @media screen and (min-width: 768px) {
+    width: 620px;
+    font-size: 24px;
+  }
+`;
+const HeroEmail = styled.div`
+  /* text-align: center; */
+  margin-bottom: 15px;
+  color: #ffb852;
+  @media screen and (min-width: 768px) {
+    font-size: 24px;
   }
 `;
 
 const HeroIconsContainer = styled.div`
   display: flex;
   flex-flow: row nowrap;
-  justify-content: space-between;
+  justify-content: center;
+  align-items: center;
+  width: 50px;
 `;
 
 const Icon = styled.a`
-  margin-left: 20px;
+  margin: 0px 21px;
 `;
 
 const Hero = () => {
@@ -37,15 +47,16 @@ const Hero = () => {
         Welcome! I am a passionate web developer looking for new opportunities.
         Feel free to reach out and maybe we can work together!
       </HeroText>
+      <HeroEmail>nablanco92@gmail.com</HeroEmail>
       <HeroIconsContainer>
         <Icon href="https://github.com/nablanco">
-          <FaGithub size={30} color="#ee9d28" />
+          <FaGithub size={37} color="#ee9d28" />
         </Icon>
         <Icon href="https://www.linkedin.com/in/nicholas-adam-blanco/">
-          <FaLinkedin size={30} color="#ee9d28" />
+          <FaLinkedin size={37} color="#ee9d28" />
         </Icon>
         <Icon href="https://www.instagram.com/nickbe92/">
-          <FaInstagram size={30} color="#ee9d28" />
+          <FaInstagram size={37} color="#ee9d28" />
         </Icon>
       </HeroIconsContainer>
     </HeroContainer>
